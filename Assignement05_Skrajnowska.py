e4e63ec3d36b7da6b7253f6522b1d13b9227cfaa
zoskr <skrajnowskaz@gmail.com>
2024-02-27 14:07:13 -0500

    All methods in BasicMathOperations are defined

---
+++ Assignement05_Skrajnowska.py
@@ -0,0 +1,33 @@
+class BasicMathOperations:
+    def _init_():
+        
+    def greetUser():
+        
+    def addNumbers():
+        
+    def preformOperations():
+        
+    def squareNumber():
+        
+    def factorial():
+        
+    def counting():
+        
+    def computeHyponenuse():
+        
+    def calculateSquare():
+        
+    def calculateHypotensuse():
+        
+    def areaOfRect():
+        
+    def power():
+        
+    def typeOfArg():
+        
+
+def main():
+    
+    
+    
+main()
